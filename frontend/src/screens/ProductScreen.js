252129a6cce6f9b11469a53ece8d0fe90dc2a60a
pratikharne <pratikharne6@gmail.com>
2021-09-20 14:27:31 +0530

    share code on github

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -18,7 +18,7 @@ export default function ProductScreen(props) {
   const addToCartHandler = () => {
     props.history.push(`/cart/${productId}?qty=${qty}`);
   };
-  return (
+  return (        
     <div>
       {loading ? (
         <LoadingBox></LoadingBox>
